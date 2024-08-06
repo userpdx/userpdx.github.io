fd305a451b46e067efbf7b0203d3180752148033
wangchengjie <wangchengjie@apusapps.com>
2024-08-06 17:25:10 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
 	envDir: './env',
-	base: '/',
+	base: './',
 	server: {
 		host: '0.0.0.0',
 		open: false,
